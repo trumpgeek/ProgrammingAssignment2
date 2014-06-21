1a9a9972dc2a1fb546634ecdb12ebfbd858c875a
trumpgeek <trumpgeek@users.noreply.github.com>
2014-06-21 11:34:13 +1000

    Updated to put more documentation on the execution of the function.
    Proof that the function seems to be working as expected.

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
 ##LPC
+## Do it like it is written in the sample just replacing 'mean'mean with 'solve'
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -12,6 +13,7 @@ makeCacheMatrix <- function(x = matrix()) {
        setInverse = setInverse,
        getInverse = getInverse)
 }
+## Doing it like it is laid out in the sample except make 'mean' to be 'solve'
 cacheSolve <- function(x, ...) {
   m <- x$getInverse()
   if(!is.null(m)) {
@@ -24,15 +26,22 @@ cacheSolve <- function(x, ...) {
   m
 }
 #########################
+## Here is an example of execution. Create a matrix called B
+##> B
+##     [,1] [,2]
+##[1,]    1    2
+##[2,]    2    1
 ## result of execution
 ##> b<-makeCacheMatrix()
+## now make it work for B
 ##> b$set(B)
 ##> cacheSolve(b)
 ##[,1]       [,2]
 ##[1,] -0.3333333  0.6666667
 ##[2,]  0.6666667 -0.3333333
+## Make sure it returns the same thing without the cacheSolve function
 ##> solve(B)
 ##[,1]       [,2]
 ##[1,] -0.3333333  0.6666667
 ##[2,]  0.6666667 -0.3333333
-#> 
+## The above shows the function appears to be correct
