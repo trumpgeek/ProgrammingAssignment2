1ba149fd79074006241f72218ad7345588c5d4ed
trumpgeek <trumpgeek@users.noreply.github.com>
2014-06-21 11:26:17 +1000

    Update cachematrix.R
    With test results.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##LPC
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) m <<- solve
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached matrix data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
+#########################
+## result of execution
+##> b<-makeCacheMatrix()
+##> b$set(B)
+##> cacheSolve(b)
+##[,1]       [,2]
+##[1,] -0.3333333  0.6666667
+##[2,]  0.6666667 -0.3333333
+##> solve(B)
+##[,1]       [,2]
+##[1,] -0.3333333  0.6666667
+##[2,]  0.6666667 -0.3333333
+#> 
